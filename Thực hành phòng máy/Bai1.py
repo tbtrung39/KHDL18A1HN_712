0645930af9d53b2e894a572a3142056bacc49c7b
unknown <mtnhue.24174600175@sv.uneti.edu.vn>
2025-02-20 05:52:07 +0700

    Nộp bài tập lab01

---
+++ Thực hành phòng máy/Bai1.py
@@ -0,0 +1,13 @@
+# Nhập thông tin sinh viên
+ma_sv = input("Nhập mã số sinh viên: ")
+ho_ten = input("Nhập họ tên sinh viên: ")
+que_quan = input("Nhập quê quán sinh viên: ")
+nam_sinh = input("Nhập năm sinh sinh viên: ")
+diem_tb = float(input("Nhập điểm trung bình các năm học: "))
+# Xuất thông tin sinh viên
+print("\nThông tin sinh viên:")
+print(f"Mã số sinh viên: {ma_sv}")
+print(f"Họ tên sinh viên: {ho_ten}")
+print(f"Quê quán sinh viên: {que_quan}")
+print(f"Năm sinh sinh viên: {nam_sinh}")
+print(f"Điểm trung bình: {diem_tb}")
