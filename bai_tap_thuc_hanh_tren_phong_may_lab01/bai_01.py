68a3fe92e787fdffcc292d6cc525f375ab5e5cde
NguyenManhTien <nmtien-24174600177@sv.uneti.edu.vn>
2025-02-20 19:04:20 +0700

    Hoàn thành bài 1,3,5,7,9,11

---
+++ bai_tap_thuc_hanh_tren_phong_may_lab01/bai_01.py
@@ -0,0 +1,13 @@
+#Nhập thông tin sinh viên
+
+ma_sinh_vien=int(input('Nhập mã sinh viên: '))
+ho_ten=input('Nhập họ tên sinh viên: ')
+que_quan=input('Nhập quê quán sinh viên: ')
+nam_sinh=int(input('Nhập năm sinh: '))
+diem_tb=float(input('Nhập điểm trung bình của sinh viên: '))
+print(ma_sinh_vien)
+print(ho_ten)
+print(que_quan)
+print(nam_sinh)
+print(diem_tb)
+
