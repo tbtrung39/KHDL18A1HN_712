c6c056d27c3065be822cf5d1a8ac99bc53e61f94
unknown <Ha_Giang_Nam>
2025-02-24 09:58:39 +0700

    đã hoàn thành LAB02

---
+++ LAB02/bài_tập_thực_hành_trên_phòng_máy/cau_05.py
@@ -0,0 +1,27 @@
+thang = int(input("Nhập vào một tháng (1-12): "))
+if thang == 1:
+    print("tháng 1")
+elif thang == 2:
+    print("tháng 2")
+elif thang == 3:
+    print("tháng 3")
+elif thang == 4:
+    print("tháng 4")
+elif thang == 5:
+    print("tháng 5")
+elif thang == 6:
+    print("tháng 6")
+elif thang == 7:
+    print("tháng 7")
+elif thang == 8:
+    print("tháng 8")
+elif thang == 9:
+    print("tháng 9")
+elif thang == 10:
+    print("tháng 10")
+elif thang == 11:
+    print("tháng 11")
+elif thang == 12:
+    print("thááng 12")
+else:
+    print("tháng không hợp lệ, hãy nhập lại")
