38845939f41ee99ef98082feca4fd734c5b454df
buithiyen2804 <btyen.24174600016@sv.uneti.edu.vn>
2025-02-24 22:23:53 +0700

    Update bai2.py

---
+++ thuc_hanh_lab01/bai2.py
@@ -0,0 +1,6 @@
+s = int(input("Nhập số giây: "))
+m = int(input("Nhập số phút: "))
+h = int(input("Nhập số giờ: "))
+d = int(input("Nhập số ngày: "))
+
+print(f"{d} ngày, {h} giờ, {m} phút, {s} giây")
