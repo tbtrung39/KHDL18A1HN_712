50a3a71deb79281151f57bf5ce7bc3c48dc30455
Trần Lương Sỹ <tlsy.24174600060@sv.uneti.edu.vn>
2025-02-26 11:10:45 +0700

    first commit

--- Bài_tập_thực_hành_1/câu_10.py
+++
@@ -1,4 +0,0 @@
-import math
-x= float(input("x="))
-f= math.log(x)/math.log(4) + math.log(2)/math.log(x)
-print('%0.2f'%(f))
--- Bài_tập_thực_hành_1/câu_12.py
+++
@@ -1,6 +0,0 @@
-import math
-a= float(input("Nhập vận tốc ban đầu:"))
-log4_5 = math.log(5)/math.log(4)
-t= a/(4*log4_5)
-T = '%0.2f'%(t)
-print("thời gian ô tô đi được cho đến lúc dừng là:", T,"(giây)")
