4d0b669281bf2964da625b4bb50646ada2046209
MinHobi <thienbao.030120@gmail.com>
2025-03-03 22:28:05 +0700

    Hoan thanh lab3

---
+++ lab_03/bai_tap_part_1/cau2.py
@@ -0,0 +1,10 @@
+n = int(input("Nhập n: "))
+print("Các số hoàn hảo nhỏ hơn", n, "là:", end=" ")
+
+for so in range(1, n):  
+    tong_uoc = 0  
+    for i in range(1, so):  
+        if so % i == 0:  
+            tong_uoc += i  
+    if tong_uoc == so:  
+        print(so, end=" ") 
---
+++ lab_03/bai_tap_part_1/cau6.py
@@ -0,0 +1,6 @@
+n = int(input("Nhập n: "))
+S = 0
+for i in range(1, n+1):
+    S = S + i**3
+print("Tổng lập phương của", n, "số đầu tiên là:", S)
+ 
