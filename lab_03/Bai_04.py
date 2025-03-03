e7e836e05aad2cbd16ec46db69f0eca0d7c299be
Dao Doan Huy Phuc <phuchuy2k6@outlook.com>
2025-03-03 22:43:39 +0700

    hoan thanh lab03

--- lab_03/Bai_04.py
+++
@@ -1,8 +0,0 @@
-n=int(input("Nhap n: "))
-for a in range(2,n+1):
-    s=0
-    for i in range (1,a+1):
-        if a%i==0:
-            s+=1
-    if s==2:
-        print(a,"\n",end="")
--- lab_03/Bai_05.py
+++
@@ -1,4 +0,0 @@
-cao=int(input("Nhap chieu cao: "))
-rong= int(input("Nhap chieu rong: "))
-for i in range(cao):
-    print("*"*rong)
