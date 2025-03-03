9d3269e77aa37ef92721cd14ddaad2052a6d1230
buiyen06 <btyen.24174600006@sv.uneti.edu.vn>
2025-03-03 14:32:23 +0700

    Delete lab_01 directory

--- lab_01/bai_10.py
+++
@@ -1,6 +0,0 @@
-import math
-
-def tinh_f_x_log(x):
-    """Tính giá trị biểu thức f(x)."""
-    return round(math.log(x, 4) + math.log(2, x), 2)
-
--- lab_01/bai_7.py
+++
@@ -1,9 +0,0 @@
-def tim_dinh_parabol(a, b, c):
-    """Tìm đỉnh của phương trình bậc 2."""
-    x = -b / (2 * a)
-    y = a * x**2 + b * x + c
-    return round(x, 2), round(y, 2)
-
-# Ví dụ sử dụng
-x, y = tim_dinh_parabol(1, -4, 3)
-print(f"Đỉnh parabol: ({x}, {y})")
