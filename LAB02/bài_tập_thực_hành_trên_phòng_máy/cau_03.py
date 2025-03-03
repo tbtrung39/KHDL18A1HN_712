127d17d3f5604d3c6ef98600ba402957c8861b58
uyen15 <ttuyen.24174600009@sv.uneti.edu.vn>
2025-03-04 00:06:44 +0700

    Nộp bài tập lab 02

---
+++ LAB02/bài_tập_thực_hành_trên_phòng_máy/cau_03.py
@@ -0,0 +1,18 @@
+print("\n\n\t\tNhập vào thứ trong tuần")
+lua_chon = int(input())
+if lua_chon == 1:
+    print("chủ nhật")
+elif lua_chon == 2:
+    print("Thứ 2")
+elif lua_chon == 3:
+    print("Thứ 3")
+elif lua_chon == 4:
+    print("Thứ 4")
+elif lua_chon == 5:
+    print("Thứ 5")
+elif lua_chon == 6:
+    print("Thứ 6")
+elif lua_chon == 7:
+    print("Thứ 7")
+else:
+    print("Thứ không hợp lệ, hãy nhập lại")
