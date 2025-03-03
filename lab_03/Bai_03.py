d4fa38762d6a4d19e88d3c5cfec4995eda57dffc
Dao Doan Huy Phuc <phuchuy2k6@outlook.com>
2025-03-03 20:31:57 +0700

    hoan thanh lab03

--- lab_03/Bai_03.py
+++ lab_03/Bai_03.py
@@ -1,30 +1,17 @@
-n=int(input("Nhap n: "))
-t=0
-for i in range(1,n+1):
-    if n%i==0:
-        t=1
-if t==2:
-    print(n,' la so nguyen to')
+n=int((input("nhap so nguyen duong n: ")))
+if n<2:
+    print("khong phai so nguyen to")
+elif n==2:
+    print("2 la so nguyen to")
 else:
-    print(n,' khong phai la so nguyen to')
-    duoi=n-1
-    for duoi in range(n-1,1,-1):
-        t=0
-        for i in range(1, duoi+1):
-            if duoi%i==0:
-                t+=1
-        if t==2:
+    for i in range(2,n):
+        if n%i!=0:
+            check=True
             break
-    tren=n+1
-    for tren in range(n+1, 100000000):
-        t=0
-        for i in range(1, tren+1):
-            if tren%i==0:
-                t+=1
-        if t==2:
+        else:
+            check=False
             break
-    if n-duoi<=tren-n:
-        print(f"snt gan nhat la {duoi}")
+    if check==True:
+        print(n,"la so nguyen to")
     else:
-        print(f"snt gan nhat la {tren}")
-
+        print(n,"khong phai so nguyen to")
---
+++ lab_03/bai_10.py
@@ -0,0 +1,8 @@
+a=int(input("nhap so nguyen duong a: "))
+if a<2:
+    print("khong the phan tich thanh thua so nguyen to")
+if a==2:
+
+
+
+
