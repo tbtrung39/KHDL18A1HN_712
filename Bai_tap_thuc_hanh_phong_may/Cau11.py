a671e675772b356e27de14e3769788f7ea80891a
MinhVu <bmvu.24174600003@sv.uneti.edu.vn>
2025-03-08 16:23:38 +0700

    Nop file lab 04

---
+++ Bai_tap_thuc_hanh_phong_may/Cau11.py
@@ -0,0 +1,28 @@
+while True:
+    print("\nMenu đồ uống:")
+    print("1. Cafe")
+    print("2. Cam vắt")
+    print("3. Nước ép cà rốt")
+    print("4. Nước lọc")
+    print("5. Nước dừa")
+    print("0. Thoat chuong trinh")
+    lua_chon = int(input("Vui lòng chọn đồ uống (1-5): "))
+    if lua_chon == 1:
+        print("Bạn đã chọn: Cafe")
+        break
+    elif lua_chon == 2:
+        print("Bạn đã chọn: Cam vắt")
+        break
+    elif lua_chon == 3:
+        print("Bạn đã chọn: Nước ép cà rốt")
+        break
+    elif lua_chon == 4:
+        print("Bạn đã chọn: Nước lọc")
+        break
+    elif lua_chon == 5:
+        print("Bạn đã chọn: Nước dừa")
+        break
+    elif lua_chon == 0:
+        print("Cam on ban!. Hen gap lai")
+    else:
+        print("Lựa chọn không hợp lệ! Vui lòng chọn số từ 1 đến 5.")
