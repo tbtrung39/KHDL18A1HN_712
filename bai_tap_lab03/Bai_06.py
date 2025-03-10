89b148a5225611b867fe37fdec1c4ca8eb7eb989
Vu Hoai Thuong <vhthuong.24174600010@sv.uneti.edu.vn>
2025-03-10 11:20:43 +0700

    đã hoàn thành lab04

--- bai_tap_lab03/Bai_06.py
+++
@@ -1,5 +0,0 @@
-n=int(input("nhap n: "))
-tong=0
-for i in range(1,n+1):
-    tong += i**3
-print(f"tong bac 3 cua {n} so nguyen dau tien la {tong}")
--- bai_tap_lab03/part 2/bai_10.py
+++
@@ -1,9 +0,0 @@
-
-n = int(input("Nhap vao n= "))
-i = 2
-while n > 1:
-    while n % i == 0:
-        print(i, "", end="") 
-        n //= i
-    i += 1
-print()
