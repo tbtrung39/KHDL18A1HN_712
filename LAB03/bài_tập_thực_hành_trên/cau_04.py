3e6575704058d9a8a2b3d6290b8b6321e875ccff
uyen15 <ttuyen.24174600009@sv.uneti.edu.vn>
2025-03-10 22:48:46 +0700

    Xóa LAB01, LAB02, LAB03, LAB04 khỏi nhánh ROOT

--- LAB03/bài_tập_thực_hành_trên/cau_04.py
+++
@@ -1,5 +0,0 @@
-=int(input('Nhập số n = '))  
-P=int(1) 
-for i in range(1,n+1): 
-    P*=i 
-print(n,'!=',P) 
--- LAB04/bai_tap_thuc_hanh_tren_phong_may/bai_07.py
+++
@@ -1,7 +0,0 @@
-a = int(input("Nhap so nguyen thu nhat: "))
-b = int(input("Nhap so nguyen thu hai: "))
-x,y = a,b
-while y!=0:
-    x,y = y, x % y
-bcnn = abs(a*b)//x
-print(f"Bo chung nho nhat cua {a} va {b} la: {bcnn}")
