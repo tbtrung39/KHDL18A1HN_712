220efda99aa88ec2c1644abc0f5c55595708b3a3
unknown <Ha_Giang_Nam>
2025-03-10 10:31:05 +0700

    Xóa LAB03

--- LAB03/cau07.py
+++
@@ -1,6 +0,0 @@
-n = int(input("Nhập n: "))
-tổng = sum(1 / i for i in range(1, n + 1))
-for _ in range(1): 
-    print(f"Tổng nghịch đảo của {n} số đầu tiên: {round(tổng, 3)}")
-else:
-    print("Đã tính xong tổng nghịch đảo!")
--- LAB03/cau08.py
+++
@@ -1,9 +0,0 @@
-while True:
-    n = int(input("Nhập n (n > 0): "))
-    if n > 0:
-        break
-    print("Vui lòng nhập số nguyên dương!")
-S1 = sum(i for i in range(1, n + 1))
-S2 = sum(2 * i + 1 for i in range(n))
-S3 = sum(2 * i for i in range(1, n + 1))
-print(f"S1 = {S1}, S2 = {S2}, S3 = {S3}")
