e5c56dc3df1431eb1cdba49e2ea79b5391c2cd5d
MinhVu <bmvu.24174600003@sv.uneti.edu.vn>
2025-03-22 14:40:53 +0700

    Nop lab 05

---
+++ Bai_tap_thuc_hanh_phong_may_lab05/Cau3.py
@@ -0,0 +1,14 @@
+# Câu 3. 
+Str = input("Nhập chuỗi nhị phân: ")
+is_binary = True
+for c in Str:
+    if c not in '01':
+        is_binary = False
+        break
+if is_binary:
+    decimal = 0
+    for i in range(len(Str)):
+        decimal += int(Str[i] * (2 ** (len(Str) - 1 - i)))
+    print("Số thập phân tương ứng: ", decimal)
+else:
+    print("Chuỗi nhập không phải nhị phân.")    
