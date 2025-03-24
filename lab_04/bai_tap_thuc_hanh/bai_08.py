27b63200ee2eff819a98e6dd3226cfce41b12247
NguyenMinhNguyet <nmnguyet.24174600053@sv.uneti.edu.vn>
2025-03-24 11:24:30 +0700

    Da hoan thanh bai tap lab05

--- lab_04/bai_tap_thuc_hanh/bai_08.py
+++
@@ -1,8 +0,0 @@
-# Cau 8.
-char = input("Nhap mot ky tu: ")
-if char != " ":
-    kytu = char[0]
-    ascii_value = ord(kytu)
-    print("Gia tri ASCII", kytu, "la", ascii_value)
-else:
-    print("Ban chua nhap ky tu nao")
--- lab_04/bai_tap_thuc_hanh/bai_09.py
+++
@@ -1,7 +0,0 @@
-# Cau 9.
-n = int(input("Nhap so nguyen: "))
-tong = 0
-while n!= 0:
-    tong += n % 10
-    n //= 10
-print("Tong hai chu so cua so vua nhap la: ", tong)
