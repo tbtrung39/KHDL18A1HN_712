79b105e3c5449ee93902c687226ee48600630c0c
NguyenMinhNguyet <nmnguyet.24174600053@sv.uneti.edu.vn>
2025-03-24 11:32:33 +0700

    Da hoan thanh bai tap lab05

--- lab_05/bai_tap_thuc_hanh/bai_13.py
+++ lab_05/bai_tap_thuc_hanh/bai_13.py
@@ -1,7 +1,14 @@
-n = int(input("Nhập số tự nhiên n: "))
-binary_str = ""
-while n > 0:
-    binary_str = str(n % 2) + binary_str 
-    n //= 2
-
-print("Chuỗi nhị phân:", binary_str if binary_str else "0")
+A = input("Nhập chuỗi A: ")
+B = input("Nhập chuỗi B: ")
+digits_A = [c for c in A if c.isdigit()]
+digits_B = [c for c in B if c.isdigit()]
+if digits_A and digits_B:
+    expression = "+".join(digits_A) + "=" + "+".join(digits_B)
+    sum_A = sum(int(c) for c in digits_A)
+    sum_B = sum(int(c) for c in digits_B)
+    if sum_A == sum_B:
+        print("Đẳng thức đúng:", expression)
+    else:
+        print("Không tồn tại cách đặt!")
+else:
+    print("Không thể tạo biểu thức!")
