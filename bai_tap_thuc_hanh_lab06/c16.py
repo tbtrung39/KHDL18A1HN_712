2356c57f45e1984aa6a6615fb60c69a9c0d5bcad
TranThanhNam <namhpp1812@gmail.com>
2025-03-31 23:42:54 +0700

    hoan thanh lab06

---
+++ bai_tap_thuc_hanh_lab06/c16.py
@@ -0,0 +1,11 @@
+X = int(input("Nhập X: "))
+Y = int(input("Nhập Y: "))
+
+ma_tran = []
+for i in range(X):
+  hang = []
+  for j in range(Y):
+    hang.append(i * j)
+  ma_tran.append(hang)
+
+print(ma_tran)
---
+++ bai_tap_thuc_hanh_lab06/c3.py
@@ -0,0 +1,20 @@
+lst2 = []
+while True:
+    num = int(input())
+    if num == 0:
+        break
+    lst2.append(num)
+
+# a) Đưa số dương ra đầu danh sách
+positives = [x for x in lst2 if x > 0]
+negatives = [x for x in lst2 if x <= 0]
+lst2 = positives + negatives
+print("Danh sách sau khi chuyển số dương lên đầu:", lst2)
+
+# b) Chèn m vào vị trí thứ 3
+m = int(input("Nhập số m: "))
+if len(lst2) >= 3:
+    lst2.insert(2, m)
+else:
+    lst2.append(m)
+print("Danh sách sau khi chèn m:", lst2)
