a45e69fd276bf45bdee32454e7bde45ca430fae0
24174600056_Nguyen_Thi_Thu_Trang_18A1 <ntttrang.24174600056@sv.uneti.edu.vn>
2025-03-31 22:44:18 +0700

    Update Câu 6.py

--- Thực hành lab_06/Câu 6.py
+++ Thực hành lab_06/Câu 6.py
@@ -1,7 +1,13 @@
 #câu 6:
 import random
-b=[]
-for i in range(1001):
-    a=random.randint(1,99999) 
-    b.append(a)
-    print(b)
+A = [random.randint(1, 99999) for _ in range(1000)]
+A_sorted_2 = A.copy()
+n = len(A_sorted_2)
+
+for i in range(n):
+    for j in range(0, n - i - 1):
+        if A_sorted_2[j] > A_sorted_2[j + 1]:
+            A_sorted_2[j], A_sorted_2[j + 1] = A_sorted_2[j + 1], A_sorted_2[j]
+
+print("10 số đầu tiên sau khi sắp xếp (sorted()):", A_sorted_1[:10])
+print("10 số đầu tiên sau khi sắp xếp (Bubble Sort):", A_sorted_2[:10])
