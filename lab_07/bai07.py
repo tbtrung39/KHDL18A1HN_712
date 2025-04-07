a026b92ea5eefe425be87fe22262d1a69ff2afe4
Bui Yen <btyen.241746000162sv.uneti.edu.vn>
2025-04-07 19:35:07 +0700

    da hoan thanh

---
+++ lab_07/bai07.py
@@ -0,0 +1,3 @@
+A = set(input("Nhập các ký tự cho tập A (cách nhau bởi dấu cách): ").split())
+B = set(input("Nhập các ký tự cho tập B (cách nhau bởi dấu cách): ").split())
+print("Các phần tử chung của A và B:", A & B)
