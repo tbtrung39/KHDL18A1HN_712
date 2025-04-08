6561f76c25da9a7772e7a5b3eadbe0f3bb997e92
quynh <quynhduongvan06@gmail.com>
2025-04-08 16:00:34 +0700

    Da hoan thanh lab05

---
+++ Bài 2. Thực hành trên phòng máy LAB05/Cau8.py
@@ -0,0 +1,8 @@
+# Câu 8. 
+Str = input("Nhập đoạn văn bản: ")
+words = Str.split()
+count = 0
+for word in words:
+    if word.isalpha():
+        count += 1
+print("Số từ đơn trong đoạn văn:", count)
