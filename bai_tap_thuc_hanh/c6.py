ba9a0a7a4b72def3d0f32b4a386040f105e00758
TranThanhNam <namhpp1812@gmail.com>
2025-04-28 16:36:31 +0700

    hoan thanh lab09

---
+++ bai_tap_thuc_hanh/c6.py
@@ -0,0 +1,9 @@
+import random
+n=int(input("Nhap n:"))
+A = list(range(1,n+1))
+result = []
+while A:
+    x = random.choice(A)
+    result.append(x)
+    A.remove(x)
+print("Hoan vi ngau nhien:",result)
