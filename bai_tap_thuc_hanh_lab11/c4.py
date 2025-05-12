257ce68ef69b96b4fcfecc21d874b1e7018de8aa
TranThanhNam <namhpp1812@gmail.com>
2025-05-13 00:24:24 +0700

    hoan thanh lab11

---
+++ bai_tap_thuc_hanh_lab11/c4.py
@@ -0,0 +1,26 @@
+def la_nguyen_to(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+def tim_uoc_nguyen_to(n):
+    uoc_nt = []
+    for i in range(2, n + 1):
+        if n % i == 0 and la_nguyen_to(i):
+            uoc_nt.append(i)
+    return uoc_nt
+
+def giai_bai_tap_4(file_in = 'Bai_2_Thuc_hanh_phong_may/f_int.dat', file_out = 'Bai_2_Thuc_hanh_phong_may/f_out.dat'):
+    with open(file_in , 'r') as f:
+        lines = f.readlines()
+    
+    with open(file_out,'w') as f:
+        for line in lines:
+            n = int(line.strip())
+            uoc = tim_uoc_nguyen_to(n)
+            f.write(' '.join(map(str, uoc)) + '\n')
+
+giai_bai_tap_4()
---
+++ bai_tap_thuc_hanh_lab11/c6.py
@@ -0,0 +1,40 @@
+def tao_file_bangso(filename='Bai_2_Thuc_hanh_phong_may/bangso.txt'):
+    noi_dung = """4
+211 133 180 5
+192 168 1 254
+11 1 11 233
+"""
+    with open(filename, 'w') as f:
+        f.write(noi_dung)
+
+def giai_bai_tap_6(file_in='bai_tap_thuc_hanh_lab11\Bangso.txt', file_odd='bai_tap_thuc_hanh_lab11\ODD.txt'):
+    with open(file_in, 'r') as f:
+        lines = f.readlines()
+
+# a. 
+    print("Dong dau tientien:", lines[1].strip())
+    print("Dong thu 33:", lines[3].strip())
+
+# b. 
+    print("\nNoi dung toan bo file:")
+    for line in lines:
+        print(line.strip())
+
+# c.
+    matrix = []
+    for line in lines[1:]:  
+        nums = list(map(int, line.strip().split()))
+        row = [num if num % 2 == 1 else 0 for num in nums]
+        matrix.append(row)
+
+    with open(file_odd, 'w') as f:
+        for row in matrix:
+            f.write(' '.join(map(str, row)) + '\n')
+
+# d.
+    with open(file_odd, 'r') as f:
+        last_line = f.readlines()[-1]
+        print("\nDong cuoicuoi ODD.txt:", last_line.strip())
+
+tao_file_bangso()
+giai_bai_tap_6()
