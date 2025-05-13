bfae483c9be3531a4ea7c23269bf64c970fd4cb0
NHƯ PHẠM THỊ HỒNG <pthnhu.24174600007@sv.uneti.edu.vn>
2025-05-14 00:53:10 +0700

    ghi những gì mình đã thực hiện

---
+++ lab_07/cau3.py
@@ -0,0 +1,11 @@
+import random
+
+tap_so = set()
+while len(tap_so) < 10:
+    so_ngau_nhien = random.randint(2, 99)
+    tap_so.add(so_ngau_nhien)
+
+print('Tap hop so:', tap_so)
+print('Phan tu nho nhat:', min(tap_so))
+print('Phan tu lon nhat:', max(tap_so))
+print('Tong cac phan tu:', sum(tap_so))
