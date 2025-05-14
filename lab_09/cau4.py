cf4de2fabe5ca94370fa17e7d9ccd9e451e1624f
MinHobi <thienbao.030120@gmail.com>
2025-05-14 19:36:21 +0700

    Hoan thanh lab_09

---
+++ lab_09/cau4.py
@@ -0,0 +1,11 @@
+def permute(arr, l ,r):
+    if l == r :
+        print(arr)
+    else:
+        for i in range (l, r+1):
+            arr[l],arr[i],arr[l]
+            permute(arr,l+1,r)
+            arr[l],arr[i]=arr[i],arr[l]
+n=int(input("Nhap so tu nhien n:"))
+arr=list(range(1,n+1))
+permute(arr,0,n-1)
