1c83cbc0205a9acab684f74e620d60c61415b8d2
uyen15 <ttuyen.24174600009@sv.uneti.edu.vn>
2025-05-15 20:37:02 +0700

    hoàn thành bài lab 08

---
+++ LAB08/cau_15.py
@@ -0,0 +1,9 @@
+n = int(input("Nhập số phần tử của list: "))
+lst = []
+for i in range(n):
+    x = int(input(f"Nhập phần tử thứ {i+1}: "))
+    lst.append(x)
+so_le = list(filter(lambda x: x % 2 != 0, lst))
+binh_phuong_le = list(map(lambda x: x**2, so_le))
+print("Danh sách các số lẻ:", so_le)
+print("Bình phương các số lẻ:", binh_phuong_le)
