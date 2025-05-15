8cd44e973e563c622701345b1c55632bb7f95f24
uyen15 <ttuyen.24174600009@sv.uneti.edu.vn>
2025-05-15 18:28:28 +0700

    Hoàn thành bài lab 12

---
+++ LAB12/cau_07.py
@@ -0,0 +1,10 @@
+from datetime import datetime, timedelta
+try:
+    d = int(input("ngay: "))
+    m = int(input("Thang: "))
+    y = int(input("Nam: "))
+    today = datetime(y,m,d)
+    tomorrow = today + timedelta(days=1)
+    print("Ngay tiep theo",tomorrow.strftime("%d-%m-%Y"))
+except ValueError as e:
+    print("Ngay khong hop le", e)
