fc43d904433819282fffe55480241bce58af2e62
uyen15 <ttuyen.24174600009@sv.uneti.edu.vn>
2025-05-15 20:14:44 +0700

    hoàn thành bài lab 10

---
+++ LAB10/cau08.py/matranvuong.py
@@ -0,0 +1,30 @@
+def nhapmatran(n):
+    print(f"Nhap ma tran {n}x{n}:")
+    matran=[]
+    for i in range(n):
+        hang=list(map(int,input(f"Hang {i+1}: ").split()))
+        while len(hang)!=n:
+            print("So phan tu khong dung, nhap lai")
+            hang=list(map(int,input(f"Hang {i+1}: ").split()))
+            matran.append(hang)
+    return matran
+
+def inmatran(matran):
+    print("Ma tran: ")
+    for hang in matran:
+        for hang in matran:
+            print(" ".joint(map(str, hang)))
+
+def chuyenvi(matran):
+    n=len(matran)
+    return [[matran[j][i] for j in range(n)]for i in range(n)]
+
+def doixung(matran):
+    n=len(matran)
+    for i in range(n):
+        for j in range(n):
+            if matran[i][j] != matran[j][i]:
+                return False
+    return True
+ 
+    
---
+++ LAB10/cau_06.py/doicoso2.py
@@ -0,0 +1,20 @@
+def loc(s):
+    taphop="0123456789ABCDEF"
+    s=s.upper()
+    return ''.join(c for c in s if c in taphop)
+def hecoso(s):
+    s=s.upper()
+    if all(c in '01' for c in s):
+        return 2
+    elif all(c in '0123456789' for c in s):
+        return 10
+    elif all(c in '0123456789ABCDEF' for c in s):
+        return 16
+    else:
+        return -1
+def he_2_sang_10(s):
+    return int(s,2)
+def he_8_sang_10(s):
+    return int(s,8)
+def he_16_sang_10(s):
+    return int(s,16)
