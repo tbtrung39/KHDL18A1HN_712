8ad5cd1c4ebf20e9f3a2ecc035b44e46cb61e030
24174600006_BUI_THI_YEN_18A1 <btyen.24174600006@sv.uneti.edu.vn>
2025-05-15 22:10:45 +0700

    Xóa

--- 31_Bui_Thi_Yen/lab_10/cau_2/my_square.py
+++
@@ -1,7 +0,0 @@
-def ChuviHinhVuong(a):
-    """Tính chu vi hình vuông"""
-    return 4 * a
-
-def DienTichHinhVuong(a):
-    """Tính diện tích hình vuông"""
-    return a * a
--- 31_Bui_Thi_Yen/lab_10/cau_5/main.py
+++
@@ -1,9 +0,0 @@
-from doicoso1 import number_conversion
-
-def main():
-    print("CHƯƠNG TRÌNH CHUYỂN ĐỔI HỆ CƠ SỐ")
-    print("--------------------------------")
-    number_conversion()
-
-if __name__ == "__main__":
-    main()
